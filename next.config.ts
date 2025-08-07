64951a55858928c9aa25c4fb21c96bacb0c06979
rifqi agnia mubarok <rifqiagniamubarok@gmail.com>
2025-08-07 10:12:51 +0700

    fixing: build n deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: ['@prisma/client', 'prisma'],
+  webpack: (config) => {
+    config.externals = [...config.externals, '@prisma/client'];
+    return config;
+  },
 };
 
 export default nextConfig;
